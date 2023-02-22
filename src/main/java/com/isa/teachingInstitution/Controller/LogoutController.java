fb789270f69fe77c75aca8f6513f67075ac3dc11
MalshaniDahanyaka <mekaladahanayaka80@gmail.com>
2023-02-22 12:41:13 +0530

    Implement endpoint for logout functionality

---
+++ src/main/java/com/isa/teachingInstitution/Controller/LogoutController.java
@@ -0,0 +1,28 @@
+package com.isa.teachingInstitution.Controller;
+
+import com.isa.teachingInstitution.Service.LogoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/logout")
+public class LogoutController {
+    @Autowired
+    private LogoutService logoutService;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping("/user")
+    public String logout() {
+        String authToken = request.getHeader("auth-token");
+        String token = authToken.substring(7);
+        System.out.println(token);
+        logoutService.invalidateToken(token);
+        return "logout success";
+    }
+
+
+}
