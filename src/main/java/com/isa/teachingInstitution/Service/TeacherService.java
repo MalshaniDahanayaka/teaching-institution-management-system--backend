7da9e838f06efba73f184181fda51b06f3481fe1
MalshaniDahanyaka <mekaladahanayaka80@gmail.com>
2023-02-28 17:45:14 +0530

    Implement logics to view assigned courses and enrolled students for a particular course

--- src/main/java/com/isa/teachingInstitution/Service/TeacherService.java
+++ src/main/java/com/isa/teachingInstitution/Service/TeacherService.java
@@ -1,16 +1,57 @@
 package com.isa.teachingInstitution.Service;
 
+import com.isa.teachingInstitution.Model.Course;
+import com.isa.teachingInstitution.Model.Request.CourseEnrollRequest;
+import com.isa.teachingInstitution.Model.Student;
 import com.isa.teachingInstitution.Model.Teacher;
+import com.isa.teachingInstitution.Repository.CourseRepository;
 import com.isa.teachingInstitution.Repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TeacherService {
     @Autowired
     private TeacherRepository teacherRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
     public Teacher getProfileData(String username){
 
         return teacherRepository.findById(username).get();
+    }
+
+    public List<Student> courseEnrolledStudents(String courseID){
+        Course course = courseRepository.findById(courseID).get();
+        List<Student> students = course.getStudents();
+        List<Student> studentList = new ArrayList<>();
+        for(Student student:students){
+            studentList.add(
+                    new Student(
+                            student.getFirstName(),
+                            student.getLastName(),
+                            student.getUsername(),
+                            "",
+                            student.getEmail(),
+                            student.getRole(),
+                            student.getStudentID()
+                    )
+            );
+        }
+        return studentList;
+    }
+
+    public Course teacherCourse(String userName){
+        Teacher teacher = teacherRepository.findById(userName).get();
+        return new Course(
+                teacher.getCourse().getCourseID(),
+                teacher.getCourse().getCourseName(),
+                teacher.getCourse().getAboutCourse(),
+                teacher.getCourse().getTimeSlot()
+        );
     }
 }
