4c94d9259cf864745bdaed3c5e62ee0c9e9e1860
MalshaniDahanyaka <mekaladahanayaka80@gmail.com>
2023-03-03 10:55:13 +0530

    Implement endpoints for unenrollment functionality and check enrollment functionality

--- src/main/java/com/isa/teachingInstitution/Controller/StudentController.java
+++ src/main/java/com/isa/teachingInstitution/Controller/StudentController.java
@@ -3,6 +3,7 @@ import com.isa.teachingInstitution.Model.Course;
 import com.isa.teachingInstitution.Model.Request.CourseEnrollRequest;
 import com.isa.teachingInstitution.Model.Student;
 import com.isa.teachingInstitution.Model.StudentCourseEnrollment;
+import com.isa.teachingInstitution.Repository.StudentCourseEnrollmentRepository;
 import com.isa.teachingInstitution.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,9 @@ import java.util.List;
 public class StudentController {
     @Autowired
     private StudentService studentService;
+
+    @Autowired
+    private StudentCourseEnrollmentRepository studentCourseEnrollmentRepository;
 
     @GetMapping("/profile/{username}")
     public Object getProfileData(@PathVariable String username){
@@ -43,11 +47,22 @@ public class StudentController {
     @GetMapping("/enrolled/courses/{userName}")
     public List<Course> getEnrolledCourses(@PathVariable String userName,
                                                @AuthenticationPrincipal UserDetails userDetails){
-
         List<Course> courses =  studentService.getEnrolledCourses(userName);
 
         return courses;
     }
 
+    @GetMapping("/{courseID}/enrolled/{username}")
+    public boolean checkEnrollment(@PathVariable String courseID, @PathVariable String username){
+
+        return studentService.checkEnrollment(courseID, username);
+    }
+
+    @DeleteMapping("/unenroll/{courseID}/{username}")
+    public ResponseEntity<?> unenrollStudent(@PathVariable String courseID, @PathVariable String username,
+                                                  @AuthenticationPrincipal UserDetails userDetails) {
+
+       return studentService.unenrollStudent(courseID, username);
+    }
 }
 
